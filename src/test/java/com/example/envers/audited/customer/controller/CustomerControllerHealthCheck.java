426265dd692801f63a596cf07cb1db5fdaf70427
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-14 00:56:08 +0200

    CustomerControllerHealthCheck

--- src/test/java/com/example/envers/audited/customer/controller/CustomerControllerHealthCheck.java
+++ src/test/java/com/example/envers/audited/customer/controller/CustomerControllerHealthCheck.java
@@ -1,16 +1,20 @@
 package com.example.envers.audited.customer.controller;
 
 import com.example.envers.audited.customer.domain.Customer;
-import com.example.envers.audited.customer.service.CustomerRepository;
+import com.example.envers.audited.customer.domain.CustomerDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by mtumilowicz on 2018-07-13.
@@ -24,17 +28,54 @@ public class CustomerControllerHealthCheck {
     @LocalServerPort
     private int port;
 
-    @Autowired
-    CustomerRepository repository;
-
     @Test
     public void save() {
-        repository.save(Customer.builder()
-                .firstName("a")
-                .build());
-        
-        System.out.println(restTemplate.getForEntity(createURLWithPort("/customers"), List.class));
+        assertThat(restTemplate
+                        .postForEntity(
+                                createURLWithPort("/customers"),
+                                new Customer(),
+                                Void.class)
+                        .getStatusCode(),
+                is(HttpStatus.OK));
     }
+
+    @Test
+    public void findAll() {
+        assertThat(restTemplate
+                        .getForEntity(
+                                createURLWithPort("/customers"),
+                                List.class)
+                        .getStatusCode(),
+                is(HttpStatus.OK));
+    }
+
+
+    @Test
+    public void update() {
+        restTemplate.put(createURLWithPort("/customers/1"), CustomerDto.builder().build());
+    }
+
+
+    @Test
+    public void findPersonById() {
+        assertThat(restTemplate
+                        .getForEntity(
+                                createURLWithPort("/customers/1"),
+                                Customer.class)
+                        .getStatusCode(),
+                is(HttpStatus.OK));
+    }
+
+    @Test
+    public void getHistoryById() {
+        assertThat(restTemplate
+                        .getForEntity(
+                                createURLWithPort("/customers/1/history"),
+                                List.class)
+                        .getStatusCode(),
+                is(HttpStatus.OK));
+    }
+
 
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
